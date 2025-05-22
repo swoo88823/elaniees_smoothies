f760275656868621a8102f4101778d3a92f486d3
swoo88823 <swjeon@cspi.co.kr>
2025-05-22 14:27:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,23 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
-import requests
 import pandas as pd
+#from snowflake.snowpark.context import get_active_session
+import requests
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
-  """
-)
+  """)
 
-# 사용자로부터 이름 입력 받기
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# Snowflake 세션 시작
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
@@ -26,27 +25,39 @@ pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
-# 과일 선택하기
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
-    , my_dataframe.to_pandas()['FRUIT_NAME'].tolist()  # Snowpark 데이터프레임을 리스트로 변환
+    , my_dataframe
     , max_selections=5
 )
 
 if ingredients_list:
-    ingredients_string=''
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+
+    ingredients_string =''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-        st.subheader(fruit_chosen + ' Nutrition Information')
+      
+        st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
+    #st.write(ingredients_string)
 
-# 주문 제출 버튼
-time_to_insert = st.button('Submit Order')
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
+    st.write(my_insert_stmt)
+    #st.stop()
+    
+    time_to_insert = st.button('Submit Order')
 
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        
+        st.success('Your Smoothie is ordered!', icon="✅")
