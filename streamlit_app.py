466017301b5ca4b3ffdbac23d73d77683b73cc83
swoo88823 <swjeon@cspi.co.kr>
2025-05-22 13:20:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -25,29 +26,14 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
-# 주문 제출 버튼
-time_to_insert = st.button('Submit Order')
+if ingredients_list:
+    ingredients_string=''
 
-if time_to_insert:
-    if ingredients_list and name_on_order:
-        # 선택된 과일들을 하나의 문자열로 결합
-        ingredients_string = ', '.join(ingredients_list)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-        # INSERT 쿼리 생성
-        my_insert_stmt = f"""
-            INSERT INTO smoothies.public.orders (ingredients, name_on_order)
-            VALUES ('{ingredients_string}', '{name_on_order}')
-        """
-        
-        # SQL 실행
-        session.sql(my_insert_stmt).collect()
 
-        # 성공 메시지
-        st.success(f"Order for {name_on_order} has been successfully placed! 🍓🍌🥭")
-    else:
-        st.warning("Please enter a name and select at least one ingredient.")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
