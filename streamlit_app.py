fe0aacecf07200db7782e8f4cfc373bfd7101d97
swoo88823 <swjeon@cspi.co.kr>
2025-05-22 13:33:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Snowflake 세션 시작
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 # 과일 선택하기
 ingredients_list = st.multiselect(
